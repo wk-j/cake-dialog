ed537f33ece26f30d4e409516adb0ecc2cef7c31
wk-j <somnuk.wk@gmail.com>
2017-02-06 01:50:11 +0700

    Changed font

--- CakeDialog/Win/InputDialog.Designer.cs
+++ CakeDialog/Win/InputDialog.Designer.cs
@@ -29,10 +29,10 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.okButton = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
             this.detailTextBox = new System.Windows.Forms.TextBox();
-            this.okButton = new System.Windows.Forms.Button();
-            this.cancelButton = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
@@ -42,42 +42,14 @@
             this.panel1.Controls.Add(this.cancelButton);
             this.panel1.Controls.Add(this.okButton);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel1.Location = new System.Drawing.Point(5, 128);
+            this.panel1.Location = new System.Drawing.Point(5, 168);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(478, 52);
+            this.panel1.Size = new System.Drawing.Size(533, 52);
             this.panel1.TabIndex = 0;
-            // 
-            // panel2
-            // 
-            this.panel2.Controls.Add(this.detailTextBox);
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel2.Location = new System.Drawing.Point(5, 5);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(478, 123);
-            this.panel2.TabIndex = 1;
-            // 
-            // detailTextBox
-            // 
-            this.detailTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.detailTextBox.Location = new System.Drawing.Point(0, 0);
-            this.detailTextBox.Multiline = true;
-            this.detailTextBox.Name = "detailTextBox";
-            this.detailTextBox.Size = new System.Drawing.Size(478, 123);
-            this.detailTextBox.TabIndex = 1;
-            // 
-            // okButton
-            // 
-            this.okButton.Location = new System.Drawing.Point(310, 18);
-            this.okButton.Name = "okButton";
-            this.okButton.Size = new System.Drawing.Size(75, 23);
-            this.okButton.TabIndex = 2;
-            this.okButton.Text = "OK";
-            this.okButton.UseVisualStyleBackColor = true;
-            this.okButton.Click += new System.EventHandler(this.okButton_Click);
             // 
             // cancelButton
             // 
-            this.cancelButton.Location = new System.Drawing.Point(391, 18);
+            this.cancelButton.Location = new System.Drawing.Point(439, 18);
             this.cancelButton.Name = "cancelButton";
             this.cancelButton.Size = new System.Drawing.Size(75, 23);
             this.cancelButton.TabIndex = 3;
@@ -85,16 +57,45 @@
             this.cancelButton.UseVisualStyleBackColor = true;
             this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
             // 
+            // okButton
+            // 
+            this.okButton.Location = new System.Drawing.Point(358, 18);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(75, 23);
+            this.okButton.TabIndex = 2;
+            this.okButton.Text = "OK";
+            this.okButton.UseVisualStyleBackColor = true;
+            this.okButton.Click += new System.EventHandler(this.okButton_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.detailTextBox);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(5, 10);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(533, 158);
+            this.panel2.TabIndex = 1;
+            // 
+            // detailTextBox
+            // 
+            this.detailTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.detailTextBox.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.detailTextBox.Location = new System.Drawing.Point(0, 0);
+            this.detailTextBox.Multiline = true;
+            this.detailTextBox.Name = "detailTextBox";
+            this.detailTextBox.Size = new System.Drawing.Size(533, 158);
+            this.detailTextBox.TabIndex = 1;
+            // 
             // InputDialog
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(488, 185);
+            this.ClientSize = new System.Drawing.Size(543, 225);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "InputDialog";
-            this.Padding = new System.Windows.Forms.Padding(5);
+            this.Padding = new System.Windows.Forms.Padding(5, 10, 5, 5);
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Title";
             this.TopMost = true;
